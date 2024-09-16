cfacfa874d3555604b5631565aebc90b9b1f14f1
deepakmeenax <deepak@cloudaeye.com>
2024-09-16 09:33:40 +0530

    sample pr

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function twoSum(nums, target) {
 }
 
 // Example usage:
-const nums = [2, 7, 11, 15]
-const target = 9
+const nums = [223, 72, 11, 15]
+const target = 92
 console.log(twoSum(nums, target)) // Output: [0, 1]
