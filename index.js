828dc49ea7ca32880b67d468377a06feb239a311
deepakmeenax <deepak@cloudaeye.com>
2024-10-03 12:12:01 +0530

    added new code

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function twoSum(nums, target) {
   const map = new Map()
+  const data = 677
   for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i]
     if (map.has(complement)) {
       return [map.get(complement), i]
+      console.log('deep')
     }
     map.set(nums[i], i)
   }
